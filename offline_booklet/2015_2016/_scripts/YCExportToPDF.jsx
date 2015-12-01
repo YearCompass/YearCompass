b49c3aa4770a8ce96ba77c0cc25b4af3f81b9f14
László Békéssy <borzos6@gmail.com>
2015-12-01 21:31:30 +0000

    PDF export script added, base preset added, base file modifications

---
+++ offline_booklet/2015_2016/_scripts/YCExportToPDF.jsx
@@ -0,0 +1,52 @@
+﻿/* YCExportToPDF.jsx
+ * ---------------------------------------------------------------------------- 
+ * Exports the active document to PDF and PDF Booklet
+ * ---------------------------------------------------------------------------- 
+ */
+
+/* Execute main function */
+main();
+
+/**
+ * Function: main
+ * ----------------------------------------------------------------------------
+ * The entry point for the pdf function
+ * @return {} Exports the active document to pdf
+ * ----------------------------------------------------------------------------
+ */
+function main(){
+    //Make certain that user interaction (display of dialogs, etc.) is turned on.
+    app.scriptPreferences.userInteractionLevel = UserInteractionLevels.interactWithAll;
+    
+    // Execute script only if there is an open document
+    if(app.documents.length > 0){
+        // Get current document
+        currentDocument = app.activeDocument;
+        // Export to PDF
+        exportPdf(currentDocument);
+        // Export to PDF in Booklet layout
+        //exportPdfBooklet(currentDocument);
+        // Save current document
+        currentDocument.save();
+    }
+    else{
+        alert("No documents are open. Please open a document and try again.");
+    }
+}
+
+/**
+ * Function: exportPdf
+ * ----------------------------------------------------------------------------
+ * Makes a copy from the base file and saves it
+ * @param {app.documents Object} (currentDocument) the current document
+ * @return {} Makes a copy from the currently active file
+ * ----------------------------------------------------------------------------
+ */
+function exportPdf(currentDocument) {
+    // Set output file name
+    var filename = String(currentDocument.fullName).replace("indd", "pdf");
+
+    // Set PDF Preset
+    var preset = app.pdfExportPresets.item("YCBasePDFPreset");
+    currentDocument.exportFile(ExportFormat.pdfType, File(filename), false, preset);
+}
--- offline_booklet/2015_2016/_scripts/readme.txt
+++ offline_booklet/2015_2016/_scripts/readme.txt
@@ -1,8 +1,15 @@
-This InDesign script loads the selected translation file, changes the texts in the base file and saves the new booklet.
+This folder contains the InDesign scripts for the YearCompass project
 To make it work
-  - copy the script to your InDesign scripts
+  - copy the scripts to your InDesign scripts
   OR
   - make an alias (shortcut) to this file in the InDesign User scripts
 
 More help:
 https://indesignsecrets.com/how-to-install-scripts-in-indesign.php
+
+=== YCCreateTranslation.jsx ===
+This InDesign script loads the selected translation file, 
+changes the texts in the base file and saves the new booklet.
+
+=== YCExportToPDF.jsx ===
+This InDesign script exports the active document to pdf
